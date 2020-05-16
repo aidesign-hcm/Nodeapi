24928d6d84662fbb533536781b860f19544d6483
aidesign-hcm <aithietke@gmail.com>
2020-05-16 12:04:37 +0700

    nodemon and morgan

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 // Import router
 const productRouter = require('./api/routes/products')
 const orderRputer = require('./api/routes/order')
 
+// App Use Libary
+app.use(morgan('dev'))
+
+/// Routes Handel Requests 
 app.use('/products', productRouter)
 app.use('/order', orderRputer)
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message : error.message
+        }
+    })
+})
 
 module.exports = app
